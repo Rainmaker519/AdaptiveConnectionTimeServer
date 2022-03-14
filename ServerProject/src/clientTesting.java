595f2e5f88e62847e6c36e6c5e867a59dba884cf
Rainmaker519 <cws4132@rit.edu>
2022-03-13 21:03:18 -0400

    Adding initial classes used for testing

---
+++ ServerProject/src/clientTesting.java
@@ -0,0 +1,26 @@
+import java.io.*;  
+import java.net.*;
+import java.util.Scanner;  
+
+public class clientTesting {
+
+	public static void main(String[] args) {
+		try{    
+			long startTime = System.currentTimeMillis();
+			Socket s=new Socket("localhost",6666);  
+			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+			System.out.println("Type one word to send!");
+			Scanner scanner = new Scanner(System.in);
+			String userTyped = scanner.nextLine();
+			scanner.close();
+			dout.writeUTF(userTyped + " " + startTime);  
+			System.out.println("Sent!");
+			dout.flush();  
+			dout.close();  
+			s.close();  
+		} 
+		catch(Exception e) {
+			System.out.println(e);
+		}  
+	}  
+}
